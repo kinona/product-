a2caabd85ebd171d2b764a2c0be3c9694be044d7
kinona <97958646+kinona@users.noreply.github.com>
2022-05-30 11:01:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int a,b,c,answer;
+cout<<"Enter the first number";
+cin>>a;
+cout<<"Enter the second number";
+cin>>b;
+cout<<"Enter the third number";
+cin>>c;
+answer=a* b * c;
+cout<<"The product of the three numbers is";
+cout<<answer;
+
+    return 0;
+}
